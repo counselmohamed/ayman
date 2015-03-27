e9b10223493d3c09dbad7e16ceac84e8a080a534
counselmohamed <mohamedjad2020@gmail.com>
2015-03-27 19:47:28 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 ﻿  <?php
-$link = mysql_connect ('localhost', 'mohamedj_memo', 'memomemo') or die ("Impossible de se connecter");
+$link = mysql_connect ('23.95.25.103', 'mohamedj_memo', 'memomemo') or die ("Impossible de se connecter");
 mysql_select_db ("mohamedj_memo")or die ("Impossible d'accéder à la base de données");
 $nom = $_POST['nom'];
 $mail = $_POST['mail'];
